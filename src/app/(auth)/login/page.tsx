83ffc1a4bb0eeaf67cd06d78392cd9a7fb65ee5c
stinkyfish1 <156199864+stinkyfish1@users.noreply.github.com>
2025-03-18 10:50:21 +0100

    added: added workability to the authentication system, still need functionality

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { LoginForm } from "./components/loginForm";
+
+const LogoutPage = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => router.push("/login"), 2000); // Redirect to login instead of home
+    }, []);
+
+    return <div>You have logged in... redirecting in a sec.</div>;
+};
+
+export default LoginForm;
